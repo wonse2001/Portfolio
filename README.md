d5ea1ef6a23a598ba155c675fbd95abd7a9fceee
wonse2001 <73517354+wonse2001@users.noreply.github.com>
2020-12-14 20:35:15 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Practice
+ web_practice
---
+++ my-project/youtube.js
@@ -0,0 +1,15 @@
+const moreBtn = document.querySelector('.info .metadata .moreBtn');
+const title = document.querySelector('.info .metadata .title');
+
+moreBtn.addEventListener('click', () => {
+    moreBtn.classList.toggle('clicked');
+    title.classList.toggle('clamp');
+});
+
+const toggleBtn = document.querySelector('.header_toggleBtn');
+const menu = document.querySelector('.header_menu');
+
+toggleBtn.addEventListener('click', () =>{
+    menu.classList.toggle('active');
+});
+
